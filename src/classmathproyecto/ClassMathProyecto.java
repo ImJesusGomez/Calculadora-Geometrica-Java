c1067dc787aeb66be2d423e55f68cc00a6073d9e
ImJesusGomez <jesusgomez.dev10@gmail.com>
2024-06-06 10:42:50 -0600

    Inicio del proyecto

---
+++ src/classmathproyecto/ClassMathProyecto.java
@@ -0,0 +1,17 @@
+
+package classmathproyecto;
+
+/**
+ *
+ * @author Microsoft Windows 10
+ */
+public class ClassMathProyecto {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
